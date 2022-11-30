f7737ef64a8b317badd9efde5205be4b02171b35
ByMaluf <brenno.maluf@gmail.com>
2022-11-30 15:44:30 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploDirectory1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ExemploDirectory1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string origem = @"c:\temp\origem";
+            string destino = @"c:\temp\destino";
+
+            try
+            {
+                //IEnumerable<string> arquivoTxt = Directory.EnumerateFiles(origem);
+                var arquivoTxt = Directory.EnumerateFiles(origem);
+
+                foreach (string arquivo in arquivoTxt) //Pode deixar do tipo var tb
+                {
+                    /* 
+                    * Recupera uma subcadeia de caracteres desta instância. 
+                    * A subcadeia de caracteres começa em uma posição de caractere
+                    * especificado e continua até o final da cadeia de caracteres.
+                    */
+
+                    string nomeArquivo = arquivo.Substring(origem.Length + 1);
+                    Directory.Move(arquivo, Path.Combine(destino, nomeArquivo));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu um erro!\n\r" + e.Message);
+            }
+
+        }
+    }
+}
